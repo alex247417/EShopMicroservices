e37185cfb440035623248e750d7c67c4d5b1f5dd
Alecsandro <alex247417@hotmail.com>
2026-07-28 10:37:55 -0300

    feat: add BuildingBlocks with CQRS abstractions and CreateProduct vertical slice

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProductHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products
+{
+
+    public record CreateProductCommad(string Name, List<string> Category, string Descripition, string ImageFile, decimal Price)
+        :IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommad, CreateProductResult>
+    {
+
+        public Task<CreateProductResult> Handle(CreateProductCommad request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+

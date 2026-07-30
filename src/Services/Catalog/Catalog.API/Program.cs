8497fafb51cb78c3e84f7347c42ef4285fd6fb10
Alecsandro <alex247417@hotmail.com>
2026-07-29 21:40:37 -0300

    feat: register MediatR and Carter dependency injection in Program.cs

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,8 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add Services to the contanier.
 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(ConfigurationBinder =>
+{
+    ConfigurationBinder.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.MapCarter();
 app.Run();

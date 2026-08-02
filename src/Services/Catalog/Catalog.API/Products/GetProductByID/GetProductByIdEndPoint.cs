361f7fec5296388f6e5f74eb06fcbd334c8140ec
Alecsandro <alex247417@hotmail.com>
2026-08-02 20:56:17 -0300

    feat: add GetProductByCategory endpoint

--- src/Services/Catalog/Catalog.API/Products/GetProductByID/GetProductByIdEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductByID/GetProductByIdEndPoint.cs
@@ -6,7 +6,7 @@
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapGet("/product/{id}", async (Guid id, ISender sender) =>
+            app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
             {
                 var result = await sender.Send(new GetProductByIdQuery(id));
 
